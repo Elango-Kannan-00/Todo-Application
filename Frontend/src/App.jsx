f4c51d4443515119f0c58a621a26cdfcd632a355
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-04-07 11:20:39 +0530

    Update API_URL to use production backend URL

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 
-const API_URL = "http://localhost:8000/todos"; // FastAPI backend
+const API_URL = "https://todo-application-1-vyoo.onrender.com"; // FastAPI backend
 
 const App = () => {
   const [todo, setTodo] = useState("");
